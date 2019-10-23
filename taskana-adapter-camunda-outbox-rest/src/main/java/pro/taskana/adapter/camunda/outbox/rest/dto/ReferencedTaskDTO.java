3b7a6f75b95f77fa19d7d5e2a612267a16afadac
Jörg Heffner <joerg.heffner@novatec-gmbh.de>
2019-10-23 14:25:57 +0200

    TSK 912,913,914 - Added TASKANATaskListener, example for task listener and outbox rest service

---
+++ taskana-adapter-camunda-outbox-rest/src/main/java/pro/taskana/adapter/camunda/outbox/rest/dto/ReferencedTaskDTO.java
@@ -0,0 +1,154 @@
+package pro.taskana.adapter.camunda.outbox.rest.dto;
+
+public class ReferencedTaskDTO {
+
+    private String id;
+    private String creationEventId;
+    private String name;
+    private String assignee;
+    private String created;
+    private String due;
+    private String description;
+    private String owner;
+    private String priority;
+    private String suspended;
+    private String systemURL;
+    private String taskDefinitionKey;
+    private String variables;
+    // extension properties
+    private String domain;
+    private String classificationKey;
+    private String workbasketKey;
+
+    public ReferencedTaskDTO() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreationEventId() {
+        return creationEventId;
+    }
+
+    public void setCreationEventId(String creationEventId) {
+        this.creationEventId = creationEventId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getDue() {
+        return due;
+    }
+
+    public void setDue(String due) {
+        this.due = due;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getSuspended() {
+        return suspended;
+    }
+
+    public void setSuspended(String suspended) {
+        this.suspended = suspended;
+    }
+
+    public String getSystemURL() {
+        return systemURL;
+    }
+
+    public void setSystemURL(String systemURL) {
+        this.systemURL = systemURL;
+    }
+
+    public String getTaskDefinitionKey() {
+        return taskDefinitionKey;
+    }
+
+    public void setTaskDefinitionKey(String taskDefinitionKey) {
+        this.taskDefinitionKey = taskDefinitionKey;
+    }
+
+    public String getVariables() {
+        return variables;
+    }
+
+    public void setVariables(String variables) {
+        this.variables = variables;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getClassificationKey() {
+        return classificationKey;
+    }
+
+    public void setClassificationKey(String classificationKey) {
+        this.classificationKey = classificationKey;
+    }
+
+    public String getWorkbasketKey() {
+        return workbasketKey;
+    }
+
+    public void setWorkbasketKey(String workbasketKey) {
+        this.workbasketKey = workbasketKey;
+    }
+
+}
---
+++ taskana-adapter-camunda-tasklistener/src/main/java/pro/taskana/adapter/camunda/tasklistener/TaskanaTaskListener.java
@@ -0,0 +1,114 @@
+package pro.taskana.adapter.camunda.tasklistener;
+
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.engine.impl.context.Context;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperties;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperty;
+
+import java.sql.*;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.logging.Logger;
+
+public class TaskanaTaskListener implements TaskListener {
+
+    private final Logger LOGGER = Logger.getLogger(this.getClass().getName());
+
+    private static TaskanaTaskListener instance = null;
+
+    public static TaskanaTaskListener getInstance() {
+        if (instance == null) {
+            instance = new TaskanaTaskListener();
+        }
+        return instance;
+    }
+
+    public void notify(DelegateTask delegateTask) {
+
+        try {
+
+            Connection connection = Context.getProcessEngineConfiguration().getDataSource().getConnection();
+
+            try {
+                switch (delegateTask.getEventName()) {
+
+                    case "create":
+                        insertCreateEvent(delegateTask, connection);
+                        break;
+                    case "complete":
+                        insertCompleteEvent(delegateTask, connection);
+                        break;
+                    case "delete":
+                        insertDeleteEvent(delegateTask, connection);
+                        break;
+                }
+
+            } finally {
+                connection.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertCreateEvent(DelegateTask delegateTask, Connection connection) {
+
+        try {
+
+            String camundaSchema = connection.getSchema();
+
+            connection.setSchema("taskana_tables");
+
+
+            String insertCreateEventSql = "INSERT INTO event_store (TYPE,CREATED,PAYLOAD) VALUES (?,?,?)";
+            Timestamp eventCreationTimestamp = Timestamp.from(Instant.now());
+
+            /** this gives us the userTask extension Properties, not yet used
+             *
+            CamundaProperties camundaProperties = delegateTask.getExecution().getBpmnModelElementInstance().getExtensionElements().getElementsQuery().filterByType(CamundaProperties.class).singleResult();
+            Collection<CamundaProperty> properties = camundaProperties.getCamundaProperties();
+
+             */
+
+            String referencedTaskJson = "{" +
+                    "id:" + "\""+ delegateTask.getId()+ "\"" + "," +
+                    "name:" + "\"" + delegateTask.getName() + "\"" + "," +
+                    "assignee:" + "\""+delegateTask.getAssignee()+"\"" + "," +
+                    "created:" + "\""+ delegateTask.getCreateTime()+ "\"" + "," +
+                    "due:" + "\"" + delegateTask.getDueDate() + "\"" + "," +
+                    "description:" + "\"" + delegateTask.getDescription() + "\"" + "," +
+                    "owner:" + "\"" + delegateTask.getOwner() + "\"" + "," +
+                    "priority:"+ "\"" + delegateTask.getPriority()+ "\"" + "," +
+                    "taskDefinitionKey:" + "\"" + delegateTask.getTaskDefinitionKey()+ "\""
+                    + "}";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(insertCreateEventSql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, delegateTask.getEventName());
+            preparedStatement.setTimestamp(2, eventCreationTimestamp);
+            preparedStatement.setString(3, referencedTaskJson);
+
+
+            preparedStatement.execute();
+
+            connection.setSchema(camundaSchema);
+
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    //TO-DO
+    private void insertDeleteEvent(DelegateTask delegateTask, Connection connection) {
+
+    }
+
+    //TO-DO
+    private void insertCompleteEvent(DelegateTask delegateTask, Connection connection) {
+
+    }
+}
+
