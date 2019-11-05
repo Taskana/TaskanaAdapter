dd40b2439b3edf324acb692d29047f10cb514540
Jörg Heffner <joerg.heffner@novatec-gmbh.de>
2019-11-05 12:59:25 +0100

    TSK-916
    -Added parselistener to automatically add taskana tasklistener to human user tasks
    -Schema and event store for taskana get created automatically now

--- taskana-adapter-camunda-tasklistener/src/main/resources/taskana-schema-postgres.sql
+++ taskana-adapter-camunda-tasklistener/src/main/resources/taskana-schema-postgres.sql
@@ -1,13 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS taskana_tables;
 
-CREATE SEQUENCE taskana_tables.event_store_id_seq
+CREATE SEQUENCE IF NOT EXISTS taskana_tables.event_store_id_seq
     INCREMENT 1
     START 1
     MINVALUE 1
     MAXVALUE 2147483647
     CACHE 1;
 
-CREATE TABLE taskana_tables.event_store
+CREATE TABLE IF NOT EXISTS taskana_tables.event_store
 (
     id integer NOT NULL DEFAULT nextval('taskana_tables.event_store_id_seq'::regclass),
     type text COLLATE pg_catalog."default",
