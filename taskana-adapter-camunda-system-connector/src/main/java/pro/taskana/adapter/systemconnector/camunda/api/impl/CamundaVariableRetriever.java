dd47ed227da09ce97eeb611e9906e20dc1a962c7
BerndBreier <33351391+BerndBreier@users.noreply.github.com>
2019-12-03 13:53:54 +0100

    TSK-969 clean up POMs and dependencies

--- taskana-adapter-camunda-system-connector/src/main/java/pro/taskana/adapter/systemconnector/camunda/api/impl/CamundaVariableRetriever.java
+++ taskana-adapter-camunda-system-connector/src/main/java/pro/taskana/adapter/systemconnector/camunda/api/impl/CamundaVariableRetriever.java
@@ -18,7 +18,7 @@ public class CamundaVariableRetriever {
     public String retrieveVariables(String taskId, String camundaSystemURL) {
         LOGGER.debug("entry to retrieveTaskVariables.  taskId = {}, CamundSystemURL = {} ", taskId, camundaSystemURL);
         String requestUrl = camundaSystemURL + CamundaSystemConnectorImpl.URL_GET_CAMUNDA_TASKS
-            + taskId + CamundaSystemConnectorImpl.URL_GET_CAMUNDA_VARIABLES;
+            + "/" + taskId + CamundaSystemConnectorImpl.URL_GET_CAMUNDA_VARIABLES;
 
         LOGGER.debug("about to retrieveTaskVariables.  requestURL = {} ", requestUrl);
         ResponseEntity<String> result = restTemplate.getForEntity(requestUrl, String.class);
