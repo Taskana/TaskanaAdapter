ef2e604a4f8474cbff608496bcab52de85f5726a
Jörg Heffner <joerg.heffner@novatec-gmbh.de>
2019-12-20 11:41:33 +0100

    -Include change requests from Holger Hagen
    -Changed context-root to /taskana-outbox
    -Changed rest-endpoint from rest/events to rest-api/events, because /rest/* is mapped to camunda servlet
    -Added model classes for CamundaTaskEvent and CamundaTaskEventList
    -Added CamundaTaskEventListResource
    -Added CamundaTaskEventListResourceAssembler
    -Adjusted all tests, methods and variables for users of the added classes

---
+++ taskana-adapter-wildfly-example/src/main/java/pro/taskana/sample/taskrouting/ExampleTaskRoutingProviderForDomainA.java
@@ -0,0 +1,28 @@
+package pro.taskana.sample.taskrouting;
+
+import pro.taskana.Task;
+import pro.taskana.TaskanaEngine;
+import pro.taskana.taskrouting.api.TaskRoutingProvider;
+
+/**
+ * This is a sample implementation of TaskRouter.
+ */
+public class ExampleTaskRoutingProviderForDomainA implements TaskRoutingProvider {
+
+    TaskanaEngine theEngine;
+
+    @Override
+    public void initialize(TaskanaEngine taskanaEngine) {
+        theEngine = taskanaEngine;
+    }
+
+    @Override
+    public String determineWorkbasketId(Task task) {
+        if ("DOMAIN_A".equals(task.getDomain())) {
+            return "WBI:100000000000000000000000000000000001";
+        } else {
+            return null;
+        }
+    }
+
+}
---
+++ taskana-adapter-wildfly-example/src/main/java/pro/taskana/sample/taskrouting/ExampleTaskRoutingProviderForDomainB.java
@@ -0,0 +1,28 @@
+package pro.taskana.sample.taskrouting;
+
+import pro.taskana.Task;
+import pro.taskana.TaskanaEngine;
+import pro.taskana.taskrouting.api.TaskRoutingProvider;
+
+/**
+ * This is a sample implementation of TaskRouter.
+ */
+public class ExampleTaskRoutingProviderForDomainB implements TaskRoutingProvider {
+
+    TaskanaEngine theEngine;
+
+    @Override
+    public void initialize(TaskanaEngine taskanaEngine) {
+        theEngine = taskanaEngine;
+    }
+
+    @Override
+    public String determineWorkbasketId(Task task) {
+        if ("DOMAIN_B".equals(task.getDomain())) {
+            return "WBI:100000000000000000000000000000000011";
+        } else {
+            return null;
+        }
+    }
+
+}
