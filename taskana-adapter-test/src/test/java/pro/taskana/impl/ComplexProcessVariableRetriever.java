50d911921aedb89daad36402c6a0b3652463ecf9
BerndBreier <33351391+BerndBreier@users.noreply.github.com>
2020-01-24 09:02:39 +0100

    Fix compile errors

--- taskana-adapter-test/src/test/java/pro/taskana/impl/ComplexProcessVariableRetriever.java
+++ taskana-adapter-test/src/test/java/pro/taskana/impl/ComplexProcessVariableRetriever.java
@@ -6,9 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class is responsible for retrieving complex process
- * variables containing Objects set by a camunda user
- * task.
+ * This class is responsible for retrieving complex process variables containing Objects set by a
+ * camunda user task.
  */
 public class ComplexProcessVariableRetriever implements JavaDelegate {
 
@@ -19,13 +18,12 @@ public class ComplexProcessVariableRetriever implements JavaDelegate {
   public void execute(DelegateExecution execution) throws Exception {
 
     ProcessVariableTestObject processVariableTestObject =
-        (ProcessVariableTestObject)execution.getVariable("attribute1");
+        (ProcessVariableTestObject) execution.getVariable("attribute1");
 
-     LOGGER.info("Successfully deserialized complex process variable"
-                     + " \"ProcessVariableTestObject\", "
-                     + "retrieving sample value via Getter-method : doubleField value = "
-                     + processVariableTestObject.getDoubleField());
-
-
+    LOGGER.info(
+        "Successfully deserialized complex process variable"
+            + " \"ProcessVariableTestObject\", "
+            + "retrieving sample value via Getter-method : doubleField value = "
+            + processVariableTestObject.getDoubleField());
   }
 }
