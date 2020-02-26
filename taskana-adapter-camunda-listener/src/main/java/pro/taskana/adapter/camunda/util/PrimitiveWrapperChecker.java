fa809e3d0cedf11f19c2db3d8d80d64bff285ac5
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2020-02-26 05:53:34 +0100

    ADPT-76 added ConstantNameCheck to checkstlye

--- taskana-adapter-camunda-listener/src/main/java/pro/taskana/adapter/camunda/util/PrimitiveWrapperChecker.java
+++ taskana-adapter-camunda-listener/src/main/java/pro/taskana/adapter/camunda/util/PrimitiveWrapperChecker.java
@@ -5,22 +5,24 @@ import java.util.Map;
 
 public class PrimitiveWrapperChecker {
 
-  private static final Map<Class<?>, Class<?>> primitiveWrapperTypeMap = new HashMap<>(8);
-
-  public static boolean isPrimitiveWrapper(Class<?> clazz) {
-
-    return primitiveWrapperTypeMap.containsKey(clazz);
-  }
+  private static final Map<Class<?>, Class<?>> PRIMITIVE_WRAPPER_TYPE_MAP = new HashMap<>(8);
 
   static {
-    primitiveWrapperTypeMap.put(Boolean.class, Boolean.TYPE);
-    primitiveWrapperTypeMap.put(Byte.class, Byte.TYPE);
-    primitiveWrapperTypeMap.put(Character.class, Character.TYPE);
-    primitiveWrapperTypeMap.put(Double.class, Double.TYPE);
-    primitiveWrapperTypeMap.put(Float.class, Float.TYPE);
-    primitiveWrapperTypeMap.put(Integer.class, Integer.TYPE);
-    primitiveWrapperTypeMap.put(Long.class, Long.TYPE);
-    primitiveWrapperTypeMap.put(Short.class, Short.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Boolean.class, Boolean.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Byte.class, Byte.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Character.class, Character.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Double.class, Double.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Float.class, Float.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Integer.class, Integer.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Long.class, Long.TYPE);
+    PRIMITIVE_WRAPPER_TYPE_MAP.put(Short.class, Short.TYPE);
   }
 
+  private PrimitiveWrapperChecker() {
+    // do not allow initalization
+  }
+
+  public static boolean isPrimitiveWrapper(Class<?> clazz) {
+    return PRIMITIVE_WRAPPER_TYPE_MAP.containsKey(clazz);
+  }
 }
