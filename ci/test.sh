e289d75141cf6a4b0222c34712f477b5f913c711
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2020-11-01 16:31:58 +0100

    ADPT-111: updated adapter to jdk11

--- ci/test.sh
+++ ci/test.sh
@@ -29,12 +29,12 @@ function main() {
   case "$1" in
   H2)
     set -x
-    mvn -q verify  -f $REL/.. -B -T 4C -am -Pcoverage -Dmaven.javadoc.skip -Dcheckstyle.skip
+    mvn -q verify -B -f $REL/.. -T 2C -Pcoverage -Dmaven.javadoc.skip -Dcheckstyle.skip
     # disabling sonarqube for PRs because it's not supported yet. See https://jira.sonarsource.com/browse/MMF-1371
     if [ -n "$2" ]; then
      #-Pcoverage to activate jacoco and test coverage reports
      # send test coverage and build information to sonarcloud
-     mvn sonar:sonar -f $REL/.. -Pcoverage -Dsonar.projectKey="$2"
+     mvn sonar:sonar -B -f $REL/.. -T 2C -Pcoverage -Dsonar.projectKey="$2"
     fi
     ;;
   esac
