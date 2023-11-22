cdbb573efd1436ff9d8088ad68cf1631ffc0e863
arolfes <arolfes@users.noreply.github.com>
2023-11-22 12:27:25 +0100

    Closes #612: add missing jaxrs and camunda properties
    * added simple Integration Tests for
      * `camunda-outbox-example-boot`
      * `taskana-adapter-camunda-spring-boot-example`
    
     * added missing properties
       * `camunda.bpm.generic-properties.properties.historyTimeToLive`
       * `resteasy.jaxrs.app.registration`
       * `resteasy.jaxrs.app.classes`

---
+++ taskana-adapter-camunda-spring-boot-example/src/test/java/pro/taskana/adapter/TaskanaAdapterApplicationTest.java
@@ -0,0 +1,14 @@
+package pro.taskana.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TaskanaAdapterApplication.class)
+class TaskanaAdapterApplicationTest {
+
+  @Test
+  void module_context_can_be_started_and_outbox_rest_events_are_fetched() {
+    // there is no test code here,
+    // because creating the configuration to run this method is the actual test
+  }
+}
